531f8d38a62111d6f9d63be7676fcab267fb7efa
Alexander Wyss <alexsilvan.wyss@gmail.com>
2020-03-20 20:58:22 +0100

    Revert "test"
    This reverts commit d7d4d740

--- src/websocketmgr.js
+++ src/websocketmgr.js
@@ -18,7 +18,7 @@ WebSocketMgr.server = null;
 /**
  * Start the websocket server
  */
-module.export = function(app) {
+module.export =  function startServer(app) {
     try {
         if (WebSocketMgr.server === null) {
             WebSocketMgr.server = new WebSocketServer({server: app});
